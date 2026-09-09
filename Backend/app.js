ccb8b2bc81e505069391edf51a057661bd20eb66
Goutham Kulall <gouthamkulall615@gmail.com>
2026-09-09 20:28:18 +0530

    added the simple implementation of the rooms users using socket.io

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ import { createServer, METHODS } from "http";
 import { Server } from "socket.io";
 import { YSocketIO } from "y-socket.io/dist/server";
 import authRoutes from "./routes/authRoutes.js";
+import { act } from "react";
 const app = express();
 app.use(express.json());
 app.use(
@@ -25,11 +26,43 @@ const io = new Server(httpServer, {
 
 const ysocketio = new YSocketIO(io);
 ysocketio.initialize();
+const userSocketMap = {};
 
 io.on("connection", (socket) => {
   console.log("Socket connected successfully", socket.id);
 
+  socket.on("join_room", ({ roomId, username }) => {
+    socket.join(roomId);
+
+    userSocketMap[socket.id] = { roomId, username };
+
+    const clientsInRoom = Array.from(
+      io.sockets.adapter.rooms.get(roomId) || [],
+    );
+
+    const activePeers = clientsInRoom.map((id) => userSocketMap[id].username);
+    io.to(roomId).emit("peers_update", activePeers);
+  });
+
+  socket.on("code_change", ({ roomId, newCode }) => {
+    socket.to(roomId).emit("receive_code", newCode);
+  });
+
   socket.on("disconnect", () => {
+    const user = userSocketMap[socket.id];
+    if (user) {
+      const { roomId } = user;
+      delete userSocketMap[socket.id];
+
+      const clientsInRoom = Array.from(
+        io.sockets.adapter.rooms.get(roomId) || [],
+      );
+      const activePeers = clientsInRoom
+        .map((id) => userSocketMap[id]?.username)
+        .filter(Boolean);
+
+      io.to(roomId).emit("peers_update", activePeers);
+    }
     console.log("Socket disconnected", socket.id);
   });
 });
